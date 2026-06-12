486906a5ab23c7340cb3a87cafaf3f3f3c5438ad
sriram <sriramsriram123654@gmail.com>
2026-06-12 10:36:45 +0530

    Add files via upload

---
+++ JAVA PROJECTS/23471A05BL_SRIRAM_SmartCampusManagement/src/scms/DBConnection.java
@@ -0,0 +1,33 @@
+package scms;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class DBConnection {
+
+    private static Connection con = null;
+
+    public static Connection getConnection() {
+
+        try {
+
+            if (con == null || con.isClosed()) {
+
+                Class.forName("com.mysql.cj.jdbc.Driver");
+
+                con = DriverManager.getConnection(
+                        "jdbc:mysql://localhost:3306/smart_campus",
+                        "root",
+                        "password");
+
+                System.out.println("Database Connected Successfully");
+            }
+
+        } catch (Exception e) {
+            System.out.println("Database Connection Error : " + e.getMessage());
+        }
+
+        return con;
+    }
+}
+
---
+++ JAVA PROJECTS/erp_management_System/src/main/java/com/example/erp_management_System/ErpManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package com.example.erp_management_System;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ErpManagementSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ErpManagementSystemApplication.class, args);
+	}
+
+}
